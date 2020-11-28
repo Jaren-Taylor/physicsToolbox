426759f43fd8b929ad9ee2c810a81384b46ce43a
unknown <Jetaylor2@Oakland.edu>
2020-11-27 23:56:39 -0500

    Added UI func

--- src/main/java/com/mycompany/physicstoolbox/Main.java
+++ src/main/java/com/mycompany/physicstoolbox/Main.java
@@ -2,39 +2,87 @@ package com.mycompany.physicstoolbox;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.beans.PropertyChangeListener;
+import java.beans.*;
 
 public class Main extends JFrame {
+
     private static final Dimension WINDOW_SIZE = new Dimension(1200, 800);
     private static final Dimension VIEWPORT_SIZE = new Dimension(800, 600);
-  
+    private static JButton substanceItem;
+   
+
     private static Substance[] allSubstances = new Substance[0];
-    
-     static class SubstanceMenu extends JPanel{
+
+     static class SubstanceMenu extends JPanel implements ActionListener {
+        public static boolean substanceAdded = false;
+        PropertyChangeSupport pcs = new PropertyChangeSupport(this);
         public JLabel menu = new JLabel("Placeholder");
-        public SubstanceMenu(){
-            add(menu);
-            setPreferredSize(new Dimension(800, 170));
+
+        public void actionPerformed(ActionEvent e) {
+         String name = ((JButton) e.getSource()).getText();
+         Substance.setCurrentlySelected(substanceByName(name));
+         
+        }
+        
+//          @Override
+//        public void propertyChange(PropertyChangeEvent event) {
+//        if (event.getNewValue() == true) {
+//            System.out.println(event.getNewValue().toString());
+//        }
+//    }
+        
+        public void rescanSubstances(){
+            if(substanceAdded){
+                substanceAdded = false;
+                this.repaint();
+            }
+        }
+
+        public SubstanceMenu() {
+            setPreferredSize(new Dimension(800, 140));
             setBorder(BorderFactory.createLineBorder(Color.yellow));
+            setLayout(new GridLayout(5, 4));
+
+            for (Substance sub : allSubstances) {
+                String name = sub.getName();
+                substanceItem = new JButton(name);
+                substanceItem.setBackground(substanceByName(name).getColor());
+                substanceItem.setMaximumSize(substanceItem.getPreferredSize());
+                add(substanceItem);
+                substanceItem.addActionListener(this);
+            }
+
+        }
+        
+        public static Substance substanceByName(String name) {
+            for (Substance sub : allSubstances) {
+                if (sub.getName().equalsIgnoreCase(name)) {
+                    return sub;
+                }
+            }
+            System.out.println("No substance found, the selected substance will remain the same.");
+            return Substance.getCurrentlySelected();
         }
     }
-    
-    
-    public static void main (String[] args) {
+
+    public static void main(String[] args) {
         Substance.loadSavedSubstances();
-        
+
         allSubstances = Substance.getSavedSubstances();
-        
+
         Substance.setCurrentlySelected(allSubstances[0]);
         Substance.setAlternateSelected(allSubstances[1]);
-        
+
         JFrame frame = new JFrame("Physics Toolbox");
-        Viewport vp = Viewport.getInstance(VIEWPORT_SIZE);
         UI ui = new UI();
+        Viewport vp = Viewport.getInstance(VIEWPORT_SIZE);
         SubstanceMenu sm = new SubstanceMenu();
         JPanel vpContainer = new JPanel(new BorderLayout());
         JPanel uiContainer = new JPanel();
@@ -42,30 +90,32 @@ public class Main extends JFrame {
         frame.setPreferredSize(WINDOW_SIZE);
         frame.setResizable(false);
         uiContainer.add(ui);
-        frame.add(uiContainer,BorderLayout.LINE_START);
+        frame.add(uiContainer, BorderLayout.LINE_START);
         vpContainer.add(vp, BorderLayout.PAGE_START);
         vpContainer.add(sm, BorderLayout.PAGE_END);
-        frame.add(vpContainer,BorderLayout.LINE_END);
-        
-        
-        
+        frame.add(vpContainer, BorderLayout.LINE_END);
+
         frame.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 Substance.saveSubstances();
             }
         });
-        
+
         frame.pack();
         frame.getContentPane().setLayout(new BorderLayout());
         frame.setVisible(true);
     }
-    
+
     public static void addSubstanceToMenu(Substance sub) {
         List<Substance> subsAsList = Arrays.asList(allSubstances);
         subsAsList.add(sub);
-        
+
         Substance[] subsAsArray = new Substance[subsAsList.size()];
         allSubstances = subsAsList.toArray(subsAsArray);
+    }
+
+    public static Substance[] getSubstances() {
+        return allSubstances;
     }
 }
--- src/main/java/com/mycompany/physicstoolbox/UI.java
+++ src/main/java/com/mycompany/physicstoolbox/UI.java
@@ -4,6 +4,8 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
+import java.util.*;
+import com.mycompany.physicstoolbox.Main.SubstanceMenu;
 
 /**
  *
@@ -15,73 +17,193 @@ public class UI extends JPanel implements ChangeListener, ItemListener {
 
     public static JLabel title = new JLabel("Substance Editor");
     public static JLabel name = new JLabel("Name");
+    public static JLabel cl = new JLabel("Color");
     public static JLabel viscosity = new JLabel("Viscosity");
     public static JLabel weight = new JLabel("Weight");
     public static JLabel density = new JLabel("Density");
     public static JLabel state = new JLabel("State");
+    public static JLabel reactivity = new JLabel("Reactivity");
+    public static JLabel rpRelation = new JLabel("Reaction --> Product");
+    public static JLabel reactant = new JLabel("Reactant");
+    public static JLabel product = new JLabel("Product");
+    public static JLabel volatilityL = new JLabel("Volatility");
     public static JRadioButton solid = new JRadioButton("Solid");
     public static JRadioButton liquid = new JRadioButton("Liquid");
     public static JRadioButton gas = new JRadioButton("Gas");
+    public static JCheckBoxMenuItem decays = new JCheckBoxMenuItem("Decay");
+    public static JCheckBoxMenuItem flammable = new JCheckBoxMenuItem("Flammable");
+
     public ButtonGroup bg = new ButtonGroup();
+    public static Substance[] substances = Main.getSubstances();
 
     public static Double vis;
     public static Double wei;
     public static Double den;
+
+    public static Double decay;
+    public static Double flammability;
+    public static Double volat;
     public static Substance.State substanceState;
 
     public static JTextField nameInput = new JTextField(10);
     JSlider v = new JSlider(0, 10);
     JSlider w = new JSlider(0, 10);
     JSlider d = new JSlider(0, 10);
+    JSlider dec = new JSlider(0, 10);
+    JSlider flam = new JSlider(0, 10);
+    JSlider volatility = new JSlider(0, 10);
 
-    public JLabel cl = new JLabel("Color");
+    //Components
+    public Rect rect = new Rect();
+    public Reactivity decayComponent = new Reactivity(decays, dec);
+    public Reactivity flammabilityComponent = new Reactivity(flammable, flam);
+    public ReactivityRect reactantComponent = new ReactivityRect(reactant);
+    public ReactivityRect productComponent = new ReactivityRect(product);
+
+    public JPanel statePanel = new JPanel();
+    public JPanel decayPanel = new JPanel();
+    public JPanel flammabiltyPanel = new JPanel();
+    public JPanel reactantPanel = new JPanel();
+    public JPanel productPanel = new JPanel();
+    public JRadioButton create = new JRadioButton("Create");
 
     public UI() {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setPreferredSize(EDITOR_SIZE);
         setBorder(BorderFactory.createLineBorder(java.awt.Color.RED));
 
-        Rect rect = new Rect();
-        JPanel statePanel = new JPanel();
         statePanel.add(solid);
+
         statePanel.add(liquid);
+
         statePanel.add(gas);
+
         statePanel.setMaximumSize(statePanel.getPreferredSize());
 
-        title.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+        decayPanel.add(decayComponent);
+
+        decayPanel.setMaximumSize(decayPanel.getPreferredSize());
+
+        flammabiltyPanel.add(flammabilityComponent);
+
+        flammabiltyPanel.setMaximumSize(flammabiltyPanel.getPreferredSize());
+
+        reactantPanel.add(reactantComponent);
+
+        reactantPanel.setMaximumSize(reactantPanel.getPreferredSize());
+
+        productPanel.add(productComponent);
+
+        productPanel.setMaximumSize(reactantPanel.getPreferredSize());
+
+        title.setFont(
+                new Font("Times New Roman", Font.PLAIN, 25));
         nameInput.setMaximumSize(nameInput.getPreferredSize());
+        create.setMaximumSize(create.getPreferredSize());
 
         add(title);
-        add(Box.createRigidArea(new Dimension(0, 10)));
+
         add(name);
+
         add(nameInput);
-        add(Box.createRigidArea(new Dimension(0, 10)));
+
         add(cl);
+
         add(rect);
+
         add(viscosity);
+
         add(v);
+
         add(weight);
+
         add(w);
+
         add(density);
+
         add(d);
+
         add(state);
+
         add(statePanel);
 
-        v.addChangeListener(this);
-        w.addChangeListener(this);
-        d.addChangeListener(this);
+        add(reactivity);
+
+        add(decayPanel);
+
+        add(flammabiltyPanel);
+
+        add(rpRelation);
+
+        add(reactantPanel);
+
+        add(productPanel);
+
+        add(volatilityL); //label
+
+        add(volatility); //slider
+
+        add(create);
+
+        v.addChangeListener(
+                this);
+        w.addChangeListener(
+                this);
+        d.addChangeListener(
+                this);
 
         rect.setAlignmentX(Component.LEFT_ALIGNMENT);
+
         v.setAlignmentX(Component.LEFT_ALIGNMENT);
+
         w.setAlignmentX(Component.LEFT_ALIGNMENT);
+
         d.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        volatility.setAlignmentX(Component.LEFT_ALIGNMENT);
+
         statePanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        solid.addItemListener(this);
-        liquid.addItemListener(this);
-        gas.addItemListener(this);
+
+        decayPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        flammabiltyPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        reactantPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        productPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        solid.addItemListener(
+                this);
+        liquid.addItemListener(
+                this);
+        gas.addItemListener(
+                this);
+        create.addItemListener(
+                this);
         bg.add(gas);
+
         bg.add(solid);
+
         bg.add(liquid);
+    }
+
+    public void createSubstance() {
+        Substance newSub = new Substance(this.rect.rc, this.nameInput.getText(), this.vis, this.wei, this.den, this.substanceState);
+        newSub.addReaction(
+                new SubstanceInteraction(Main.SubstanceMenu.substanceByName(
+                        reactantComponent.dropBox.getName()),
+                        Main.SubstanceMenu.substanceByName(productComponent.dropBox.getSelectedItem().toString()),
+                        SubstanceInteraction.ReactionOutcome.CHANGED,
+                        SubstanceInteraction.ReactionOutcome.UNCHANGED,
+                        this.volat
+                ));
+        Substance.addCustomSubstance(newSub);
+        Main.SubstanceMenu.substanceAdded = true;
+//        JButton substanceItem;
+//        substanceItem = new JButton(newSub.getName());
+//        substanceItem.setBackground(newSub.getColor());
+//        substanceItem.setMaximumSize(substanceItem.getPreferredSize());
+//        Main.SubstanceMenu.addNewSubToMenu(substanceItem);
     }
 
     @Override
@@ -89,34 +211,38 @@ public class UI extends JPanel implements ChangeListener, ItemListener {
         this.vis = Double.valueOf(v.getValue()) / 10;
         this.wei = Double.valueOf(w.getValue()) / 10;
         this.den = Double.valueOf(d.getValue()) / 10;
+        this.decay = Double.valueOf(v.getValue()) / 10;
+        this.flammability = Double.valueOf(w.getValue()) / 10;
+        this.volat = Double.valueOf(volatility.getValue()) / 10;
     }
 
     //buggy, we need a way to deselect the buttons whenever we click them, but we only want to deselect the buttons that weren't just clicked. will use action listeners instead.
     @Override
     public void itemStateChanged(ItemEvent e) {
-        String[] button = e.getItem().toString().split(",text=");
-        String currSelected = button[button.length-1].toString().substring(0, button[1].length() - 1);
-        
-  
-        
-       switch (currSelected){
-               case "Gas":
-                   bg.clearSelection();
-                   this.substanceState = Substance.State.GAS;
-                   break;
-                   
-               case "Solid":
-                   bg.clearSelection();
-                   this.substanceState = Substance.State.SOLID;
-                   break;
-                   
-               case "Liquid":
-                   bg.clearSelection();
-                   this.substanceState = Substance.State.LIQUID;
-                   break;
-                   
-                       }
-       System.out.println(currSelected);
+        String button = ((JRadioButton) e.getSource()).getText();
+        System.out.println(button);
+
+        switch (button) {
+            case "Gas":
+                bg.clearSelection();
+                this.substanceState = Substance.State.GAS;
+                break;
+
+            case "Solid":
+                bg.clearSelection();
+                this.substanceState = Substance.State.SOLID;
+                break;
+
+            case "Liquid":
+                bg.clearSelection();
+                this.substanceState = Substance.State.LIQUID;
+                break;
+
+            case "Create":
+                createSubstance();
+                break;
+
+        }
     }
 
     class Rect extends JPanel implements ChangeListener {
@@ -149,9 +275,71 @@ public class UI extends JPanel implements ChangeListener, ItemListener {
             g.setColor(rc); //set color based on RGB sliders
             g.fillRect(0, 10, 50, 50);
         }
+
+        public Color getColor() {
+            return this.rc;
+        }
+    }
+
+    class ReactivityRect extends JPanel implements ItemListener {
+
+        Substance selectedSub;
+        JComboBox dropBox;
+
+        public ReactivityRect(JLabel type) {
+            add(Box.createHorizontalStrut(5));
+            add(type);
+            dropBox = new JComboBox(initSubs(substances));
+            add(dropBox);
+
+            dropBox.addItemListener(this);
+
+        }
+
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawRect(0, 10, 10, 10);
+            if (this.selectedSub != null) {
+                g.setColor(this.selectedSub.getColor());
+            } else {
+                g.setColor(Color.BLACK);
+            }
+            g.fillRect(0, 10, 10, 10);
+        }
+
+        public String[] initSubs(Substance[] substances) {
+            ArrayList<String> subs = new ArrayList<>();
+
+            for (Substance sub : substances) {
+                subs.add(sub.getName());
+            }
+
+            String[] newSubs = subs.toArray(new String[0]);
+            return newSubs;
+        }
+
+        public void itemStateChanged(ItemEvent e) {
+            // if the state combobox is changed 
+            if (e.getSource() == dropBox) {
+                String substanceName = dropBox.getSelectedItem().toString();
+
+                for (Substance sub : substances) {
+                    if (sub.getName().equalsIgnoreCase(substanceName)) {
+                        this.selectedSub = sub;
+                        this.repaint();
+                    }
+                }
+
+            }
+        }
     }
 
     class Reactivity extends JPanel {
+
+        public Reactivity(JCheckBoxMenuItem item, JSlider slider) {
+            add(item);
+            add(slider);
+        }
 
     }
 
